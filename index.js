246749478a0feb8f39152e2a9ac3c415953dd0c2
Shawn A Hensley <sahensley@users.noreply.github.com>
2022-10-25 21:02:55 -0500

    remove marvin's url

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const REDIRECT_HASHTABLE = {
   dest: { url: "taikajuoma.ovh/wiki/Desters", protocol: "https:" },
   ggrmaps: { url: "tnsp.org/maps/", protocol: "https:" },
   ggrtf: { url: "tnsp.org/~ccr/ggrtf/", protocol: "https:" },
-  tazliel: { url: "sites.google.com/site/battazliel/", protocol: "https:" },
-  marvin: { url: "batmarvin.000webhostapp.com", protocol: "either" }
+  tazliel: { url: "sites.google.com/site/battazliel/", protocol: "https:" }
 };
 
 /**
